12f34ccdb91df683e15f6cda363fab9971136625
FranRK <franciscorecaldee@gmail.com>
2026-04-05 01:13:51 -0300

    Último commit del nivel 1. Cambios menores en css y html.

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ const path = require('path'); //esto para que no falle en otro sistema operativo
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
+
 // Ruta raíz
 app.get('/', (req, res) => {
   // __dirname es una variable de Node que tiene la ruta de tu carpeta actual
-  /*se hace path.join para poder acceder tanto en linux como en windows, ya que ambos manejan diferente las 
-    barras. 
-    __dirname, es para manejar concretamente a la ruta donde esta index.html. Ya que sendFile se maneja con
-    dirrecciones absolutas.*/ 
+        /*se hace path.join para poder acceder tanto en linux como en windows, ya que ambos manejan diferente las 
+        barras. 
+        __dirname, es para manejar concretamente a la ruta donde esta index.html. Ya que sendFile se maneja con
+        dirrecciones absolutas.*/ 
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 //Punto 1.2. Manejo de elementos estaticos mediante express.static: https://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 /*Todo lo que se acceda a public, esta estara invisible en la url. Todo lo accederemos de manera directa con los links
 Si tengo alog llamado index.html dentro de esto, sera a donde accederemos de manera raíz.*/
-
-
 // Middleware básico para manejo de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -30,14 +34,8 @@ app.use((err, req, res, next) => {
 //agregar acerca y contacto. Debería de hacer una página html igual. 
 app.get('/acerca', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'acerca.html'));
-
 });
 app.get('/contacto', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'contacto.html'));
-});
-// Iniciar servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 }); 
 
